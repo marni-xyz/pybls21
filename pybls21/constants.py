b5d0b466a9986f72c26179c424710954cd2b22c4
marni-xyz <xyellowmanx@gmx.de>
2026-04-20 22:24:24 +0200

    Optimization
    Extended comments, removed duplicate for StateFILTER

--- pybls21/constants.py
+++ pybls21/constants.py
@@ -18,22 +18,22 @@ IR_CurTEMP_SuAirOut = 2
 IR_CurRH_Int = 10
 IR_SuRPM = 23
 IR_ExRPM = 24
-IR_StateFILTER = 31
+IR_StateFILTER = 31 # filter status (clean, error, replacement)
 IR_VerMAIN_FMW_start = 34
 IR_VerMAIN_FMW_end = 36
 IR_DeviceTYPE = 37
 IR_ALARM = 38
 
 # MaNi additions
+CL_TIMER = 1  # main timer status
+CL_WEEK = 2  # schedule mode status
+
 IR_CurTEMP_ExAirIn = 3 # temperature for ingoing fresh air
 IR_CurTEMP_ExAirOut = 4 # temperature for outgoing air
 IR_CurFILTER_TIMER = 28 # days until filters needs cleaning (address 27 refers to hours&minutes)
-IR_CurFILTER_STATUS = 31 # filter status (clean, error, replacement)
 IR_CurSuPRESS = 21 # air pressure for ingoing fresh air
 IR_CurExPRESS = 22 # air pressure for outgoing air
-CL_TIMER = 1  # main timer status
-IR_CurTIMER_TIME_MIN = 25 # countdown of main timer
-IR_CurTIMER_TIME_HRS = 26 # countdown of main timer
-CL_WEEK = 2  # schedule mode status
+IR_CurTIMER_TIME_MIN = 25 # countdown of main timer (25 is minutes and seconds)
+IR_CurTIMER_TIME_HRS = 26 # countdown of main timer (26 is hours)
 IR_CurWeekSpeed = 32 # speed from current timeframe of schedule mode
 # EO MaNi additions
